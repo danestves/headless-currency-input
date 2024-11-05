2a06f0aae0108bf080e35f1570ab287eb5fdf72a
Daniel Esteves <me+github@danestves.com>
2024-11-05 16:31:39 -0400

    feat: make custom type from customInput works (#5)
    * feat: better function to set caret position
    
    * ci: remove deprecated husky line
    
    * feat: make custom type from customInput works
    
    ---------
    
    Co-authored-by: danestves <danestves@users.noreply.github.com>

--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,23 @@
 /**
  * @see https://github.com/i18n-components/i18n-components/blob/main/packages/input-number/src/helpers.ts#L2
  */
-export function setCaretPosition(el: HTMLInputElement, caretPos: number): boolean {
+export function setCaretPosition(el: HTMLInputElement, caretPos = 0): boolean {
+	// biome-ignore lint/correctness/noSelfAssign: comes from the reference
+	el.value = el.value;
+	// ^ this is used to not only get "focus", but
+	// to make sure we don't have it everything -selected-
+	// (it causes an issue in chrome, and having it doesn't hurt any other browser)
+
 	if (el !== null) {
+		if (el.selectionStart || el.selectionStart === 0) {
+			el.focus();
+			el.setSelectionRange(caretPos, caretPos);
+			return true;
+		}
+
+		// fail city, fortunately this never happens (as far as I've tested) :)
 		el.focus();
-		el.setSelectionRange(caretPos, caretPos);
-		return true;
+		return false;
 	}
 
 	return false;
